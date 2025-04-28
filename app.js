c5487cb3b3a4ab0537f5c5310bacf4fb5f0a53e8
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-04-28 13:49:30 +0100

    task 2 complete

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
-const {getApi} = require('./src/controllers/controllers')
+const {getApi, getTopics} = require('./src/controllers/controllers')
 
 
 
 app.use(express.json());
 
-app.get('/api', getApi)
+app.get('/api', getApi);
 
+app.get('/api/topics', getTopics )
+
+
+app.all('/*splat', (req, res)=> {
+    res.status(404).send({msg: 'invalid url!'})
+})
 module.exports = app;
--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,7 +1,12 @@
-const endpoints = require('../../endpoints.json');
+const endpoints = require("../../endpoints.json");
+const { selectTopics } = require("../models/models");
 
 exports.getApi = (request, response) => {
-    response.status(200).send({endpoints});
-}
+  response.status(200).send({ endpoints });
+};
 
-// test to see the github branch is working
+exports.getTopics = (req, res) => {
+  return selectTopics().then((topics) => {
+    res.status(200).send({ topics });
+  });
+};
